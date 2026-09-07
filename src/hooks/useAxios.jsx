6be8becd1d7bf6e5910d392179419f0243844121
Md.Redwan <150443639+Re1354@users.noreply.github.com>
2026-09-07 12:50:13 +0600

    Update Axios base URL to point to production server

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://zap-shift-server-bay-eight.vercel.app',
 });
 
 const useAxios = () => {
